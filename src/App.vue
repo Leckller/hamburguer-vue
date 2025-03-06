9b41e5e65dd82e26fafa93cc677f7d89b5672f36
Gusttavo Ruy <7leckler@gmail.com>
2025-03-06 17:14:20 -0300

    feat: setup vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <TituloH1/>
+  <CondicionamentoFisico />
+</template>
+
+<script>
+
+import TituloH1 from './components/TituloH1.vue'
+import CondicionamentoFisico from './components/CondicionamentoFisico.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TituloH1,
+    CondicionamentoFisico
+  }
+
+}
+
+</script>
---
+++ src/components/TituloH1.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+      <h1>Olá Vue!</h1>
+      <p>{{ frase }}</p>
+    </header>
+</template>
+
+<script>
+
+export default {
+  name: 'TituloH1',
+  data () {
+    return {
+      frase: 'Um começo incrivel!'
+    }
+  },
+  created () {
+    setTimeout(() => {
+      this.frase = 'Um fim terrivel.'
+    }, 1000)
+  },
+  mounted () {
+    setTimeout(() => {
+      this.frase = 'Uma nova chance!'
+    }, 2000)
+  }
+}
+
+</script>
